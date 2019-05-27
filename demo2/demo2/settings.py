a902c60a272049b4eaadc6f026feb7d4e177191c
Administrator <123456@qq.com>
2019-05-27 19:38:24 +0800

    demo2 setting

--- demo2/demo2/settings.py
+++ demo2/demo2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@kbgis0xzhflixx!1fb-oxl2=0xnm7nq6*chx%=2qpahbg6k0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
